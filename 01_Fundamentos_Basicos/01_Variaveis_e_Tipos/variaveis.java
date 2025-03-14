8f30ef64e8bcb27ed04a296ac13e9de358d787dd
Francisco_Dougllas <franciscodouglas77@outlook.com>
2025-03-14 13:47:47 -0300

    -----

---
+++ 01_Fundamentos_Basicos/01_Variaveis_e_Tipos/variaveis.java
@@ -0,0 +1,16 @@
+//Criando Variaveis em Java
+
+public class variaveis{
+    public static void main(String[] args){
+        int farinha = 2;
+
+        System.out.println("A receita requer: " + farinha + " xicaras de farinha");
+    }
+}
+
+/* Oque são Variaveis
+ * Variaveis são espaços de memória que reservamos para armazenar valores
+ * Variaveis podem ser do mesmo tipo ou de tipos diferentes
+ * Variaveis podem ser declaradas com ou sem valores
+ * Variaveis podem ser inicializadas com valores ou não
+ */
